e433ad8778dd7fe98bd309bf848aa05345d7b378
mirkovana <mana81098@gmail.com>
2020-11-30 17:20:22 +0100

    Dodavanje, izmena, brisanje i izlistavanje svih ponuda

--- demo/src/main/java/ktsnwt_tim8/demo/controller/OfferController.java
+++ demo/src/main/java/ktsnwt_tim8/demo/controller/OfferController.java
@@ -1,10 +1,108 @@
 package ktsnwt_tim8.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javassist.NotFoundException;
+import ktsnwt_tim8.demo.dto.OfferDTO;
+import ktsnwt_tim8.demo.dto.PostDTO;
+import ktsnwt_tim8.demo.model.Offer;
+import ktsnwt_tim8.demo.model.OfferImage;
+import ktsnwt_tim8.demo.model.Post;
+import ktsnwt_tim8.demo.model.Subcategory;
+import ktsnwt_tim8.demo.repository.OfferRepository;
+import ktsnwt_tim8.demo.service.OfferImageService;
+import ktsnwt_tim8.demo.service.OfferService;
+import ktsnwt_tim8.demo.service.PostService;
+import ktsnwt_tim8.demo.service.SubcategoryService;
 
 @RestController
 @RequestMapping(value = "/api/offers")
 public class OfferController {
+	
+	@Autowired
+	private OfferRepository repository;
+	
+	@Autowired
+	private OfferService service;
+	
+	@Autowired
+	private PostService servicePost;
+	
+	@Autowired
+	private OfferImageService serviceOfferImage;
+	
+	@Autowired
+	private SubcategoryService serviceSubcategory;
+	
+	/*ISPISIVANJE SVIH PONUDA*/
+	@GetMapping
+	public List<Offer> getAllOffers() {
+		List<Offer> listOffers = service.listAll();
+		return listOffers;
+	}
+	
+	/*DODAVANJE NOVE PONUDE*/
+	@PostMapping(value = "/{idSubcategory}", consumes = "application/json")
+	public ResponseEntity<OfferDTO> saveOffer(@PathVariable Long idSubcategory, @RequestBody OfferDTO offerDTO) {
+		Offer offer = new Offer();
+		offer.setDescription(offerDTO.getDescription());
+		offer.setLat(offerDTO.getLat());
+		offer.setLon(offerDTO.getLon());
+		offer.setTitle(offerDTO.getTitle());
+		Subcategory subcategory = serviceSubcategory.get(idSubcategory);
+		offer.setSubcategory(subcategory);
+		
+		offer = service.save(offer);
+		return new ResponseEntity<>(new OfferDTO(offer), HttpStatus.CREATED);
+	}
+	
+	/*IZMENA PONUDE*/
+	@PutMapping(value = "/{idOffer}", consumes = "application/json")
+	public Offer updateOffer(@PathVariable Long idOffer, @RequestBody OfferDTO offerUpdated)
+			throws NotFoundException {
+		return repository.findById(idOffer).map(offer -> {
+			//offer.setAvgRating(offerUpdated.getAvgRating());
+			offer.setDescription(offerUpdated.getDescription());
+			//offer.setLat(offerUpdated.getLat());
+			//offer.setLon(offerUpdated.getLon());
+			//offer.setNmbOfRatings(offerUpdated.getNmbOfRatings());
+			offer.setTitle(offerUpdated.getTitle());
+			return repository.save(offer);
+		}).orElseThrow(() -> new NotFoundException("Offer not found with id " + idOffer));
+
+	}
+	
+	/*BRISANJE PONUDE*/
+	@DeleteMapping(value = "/{idOffer}")
+	public List<Offer> deleteOffer(@PathVariable Long idOffer) {
+		Offer offer = service.get(idOffer);
+		List<Offer> offers = service.listAll();
+
+		offers.remove(offer);
+		//brisanje slika vezanih za ponudu i postova
+		List<Post> posts = servicePost.findAllByOffer(offer);
+		for(Post p : posts) {
+			servicePost.delete(p.getID());
+		}
+		List<OfferImage> offerimages = serviceOfferImage.findAllByOffer(offer);
+		for(OfferImage oi : offerimages) {
+			serviceOfferImage.delete(oi.getID());
+		}
+		service.delete(idOffer);
+
+		return offers;
+	}
 
 }
--- demo/src/main/java/ktsnwt_tim8/demo/dto/OfferDTO.java
+++ demo/src/main/java/ktsnwt_tim8/demo/dto/OfferDTO.java
@@ -1,10 +1,69 @@
 package ktsnwt_tim8.demo.dto;
 
 import ktsnwt_tim8.demo.model.Offer;
+import ktsnwt_tim8.demo.model.Subcategory;
 
 public class OfferDTO {
-
+	private String title;
+	private String description;
+	private double avgRating;
+	private int nmbOfRatings;
+	private double lat;
+	private double lon;
+	private SubcategoryDTO subcategory;
 	
-	public OfferDTO(Offer offer) {}
 	public OfferDTO() {}
+	
+	public OfferDTO(Offer offer) {
+		this.title = offer.getTitle();
+		this.description = offer.getDescription();
+		this.avgRating = offer.getAvgRating();
+		this.nmbOfRatings = offer.getNmbOfRatings();
+		this.lat = offer.getLat();
+		this.lon = offer.getLon();
+		this.subcategory = new SubcategoryDTO(offer.getSubcategory());
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getAvgRating() {
+		return avgRating;
+	}
+	public void setAvgRating(double avgRating) {
+		this.avgRating = avgRating;
+	}
+	public int getNmbOfRatings() {
+		return nmbOfRatings;
+	}
+	public void setNmbOfRatings(int nmbOfRatings) {
+		this.nmbOfRatings = nmbOfRatings;
+	}
+	public double getLat() {
+		return lat;
+	}
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+	public double getLon() {
+		return lon;
+	}
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+	public SubcategoryDTO getSubcategory() {
+		return subcategory;
+	}
+	public void setSubcategory(SubcategoryDTO subcategory) {
+		this.subcategory = subcategory;
+	}
 }
