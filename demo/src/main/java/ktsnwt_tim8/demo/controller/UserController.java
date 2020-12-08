014be8f7f03a3f38eded50a1431c1f1043085193
GDBxNS <51819374+GDBxNS@users.noreply.github.com>
2020-12-09 00:40:43 +0100

    Added URI to getSubscriptions
    Added URI to getSubscriptions

--- demo/src/main/java/ktsnwt_tim8/demo/controller/UserController.java
+++ demo/src/main/java/ktsnwt_tim8/demo/controller/UserController.java
@@ -59,7 +59,7 @@ public class UserController {
 	
 	
 	@PreAuthorize("hasRole('ROLE_USER')")
-	@GetMapping
+	@GetMapping("/getSubscriptions")
 	public ResponseEntity<Page<OfferDTO>> getUserSubscriptions(Pageable page){//
 		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		
