4a9cc7f67563d65e7c4e8782262012487b4cc5b9
vsmiljana <smiljana.vojvodic@gmail.com>
2020-12-26 08:52:03 +0100

    repository integration tests for comment and rating

---
+++ demo/src/test/java/ktsnwt_tim8/demo/repository/CommentRepositoryIntegrationTest.java
@@ -0,0 +1,44 @@
+package ktsnwt_tim8.demo.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import ktsnwt_tim8.demo.constants.Constants;
+import ktsnwt_tim8.demo.model.Comment;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:test.properties")
+public class CommentRepositoryIntegrationTest {
+
+	@Autowired
+	private CommentRepository commentRepository;
+	
+	//Page<Comment> findAllByOfferID(Long id, Pageable page);
+
+	@Test
+	public void testFindAllByOfferID() {
+		Pageable pageable = PageRequest.of(Constants.PAGEABLE_PAGE, Constants.PAGEABLE_SIZE);
+		Page<Comment> page = commentRepository.findAllByOfferID(1L, pageable);
+		
+		assertEquals(page.getContent().size(), 2);
+	}
+	
+	@Test
+	public void testFindAllByOfferIDBadID() {
+		Pageable pageable = PageRequest.of(Constants.PAGEABLE_PAGE, Constants.PAGEABLE_SIZE);
+		Page<Comment> page = commentRepository.findAllByOfferID(Constants.BAD_ID, pageable);
+		
+		assertEquals(page.getContent().size(), 0);
+	}
+	
+}
---
+++ demo/src/test/java/ktsnwt_tim8/demo/repository/RatingRepositoryIntegrationTest.java
@@ -0,0 +1,69 @@
+package ktsnwt_tim8.demo.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import ktsnwt_tim8.demo.constants.Constants;
+import ktsnwt_tim8.demo.model.Rating;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:test.properties")
+public class RatingRepositoryIntegrationTest {
+
+	@Autowired
+	private RatingRepository ratingRepository;
+	
+	//Page<Rating> findAllByOfferID(Long id, Pageable page);
+
+	//Rating findOneByUserIDAndOfferID(Long id, Long id2);
+
+	@Test
+	public void testFindAllByOfferID() {
+		Pageable pageable = PageRequest.of(Constants.PAGEABLE_PAGE, Constants.PAGEABLE_SIZE);
+		Page<Rating> page = ratingRepository.findAllByOfferID(1L, pageable);
+		
+		assertEquals(page.getContent().size(), 3);	
+	}
+	
+
+	@Test
+	public void testFindAllByOfferIDBadID() {
+		Pageable pageable = PageRequest.of(Constants.PAGEABLE_PAGE, Constants.PAGEABLE_SIZE);
+		Page<Rating> page = ratingRepository.findAllByOfferID(Constants.BAD_ID, pageable);
+		
+		assertEquals(page.getContent().size(), 0);	
+	}
+	
+	@Test
+	public void testFindOneByUserIDAndOfferIDExisting() {
+		
+		Rating r = ratingRepository.findOneByUserIDAndOfferID(2L, 1L);
+		
+		assertNotNull(r);
+		
+	}
+	
+	@Test
+	public void testFindOneByUserIDAndOfferIDNonExisting() {
+		
+		Rating r = ratingRepository.findOneByUserIDAndOfferID(2L, Constants.BAD_ID);
+		
+		assertNull(r);
+		
+	}
+	
+	
+	
+}
