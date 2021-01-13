205082450215b4056df2fb6fdc95e2fbf6552d97
vsmiljana <smiljana.vojvodic@gmail.com>
2021-01-13 15:53:30 +0100

    creating components, updating homepage

---
+++ Projekat-front/project-front/src/app/main-offers-view/main-offers-view.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Offer, Page } from '../models/Offer';
+import { OfferInfoService } from '../services/offer-info.service';
+
+@Component({
+  selector: 'app-main-offers-view',
+  templateUrl: './main-offers-view.component.html',
+  styleUrls: ['./main-offers-view.component.scss']
+})
+export class MainOffersViewComponent implements OnInit {
+  
+  offersPage: Page = new Page(0, 0, []);
+
+  constructor(private offerInfoService: OfferInfoService) { 
+
+  }
+
+  ngOnInit() {
+    this.offerInfoService.getOffers().subscribe(data => {
+      this.offersPage = data;
+  });    
+  }
+  
+}
